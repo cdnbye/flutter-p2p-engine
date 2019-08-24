8cb0a608e8c2277dadddea04f97c5918b4b1d549
mjl0602 <675531696@qq.com>
2019-08-24 17:34:05 +0800

    更新了文件结构

--- example/lib/pages/customVideoPage.dart
+++ example/lib/pages/customVideoPage.dart
@@ -1,4 +1,5 @@
-import 'package:cdnbye_example/pages/tapped.dart';
+import 'package:cdnbye_example/views/tapped.dart';
+import 'package:cdnbye_example/views/videoPlayer.dart';
 import 'package:flutter/material.dart';
 
 import 'package:video_player/video_player.dart';
@@ -49,7 +50,7 @@ class _CustomVideoPlayerPageState extends State<CustomVideoPlayerPage> {
         setState(() {});
       },
     );
-    _version = await Cdnbye.platformVersion;              // sdk version
+    _version = await Cdnbye.platformVersion; // sdk version
   }
 
   _updateData() async {
@@ -67,7 +68,8 @@ class _CustomVideoPlayerPageState extends State<CustomVideoPlayerPage> {
 
   _loadVideo() async {
 //    var url = 'https://iqiyi.com-t-iqiyi.com/20190722/5120_0f9eec31/index.m3u8';
-    var url = 'http://hefeng.live.tempsource.cjyun.org/videotmp/s10100-hftv.m3u8';
+    var url =
+        'http://hefeng.live.tempsource.cjyun.org/videotmp/s10100-hftv.m3u8';
     print('Original URL: $url');
     url = await Cdnbye.parseStreamURL(url);
     print('Parsed URL: $url');
@@ -91,7 +93,6 @@ class _CustomVideoPlayerPageState extends State<CustomVideoPlayerPage> {
     } catch (e) {
       print('$e');
     }
-
   }
 
   String _info = '';
@@ -117,119 +118,122 @@ class _CustomVideoPlayerPageState extends State<CustomVideoPlayerPage> {
       );
     }
 
+    Widget topVideo = Container(
+      color: Colors.black,
+      child: AspectRatio(
+        aspectRatio: 16 / 9.0,
+        child: VideoPlayerWidget(
+          video: video,
+          aspectRatio: aspectRatio,
+          videoDuration: videoDuration,
+          videoPositon: position,
+          isPlaying: isplay,
+          showToolLayer: showUi,
+          onTap: () async {
+            if (!vpController.value.initialized) {
+              return;
+            }
+            showUi = !showUi;
+            setState(() {});
+            if (showUi && isplay) {
+              await Future.delayed(Duration(seconds: 6));
+              showUi = false;
+              setState(() {});
+            }
+          },
+          onTapPlay: () async {
+            if (!vpController.value.initialized) {
+              return;
+            }
+            if (isplay) {
+              vpController.pause();
+            } else {
+              vpController.play();
+            }
+            isplay = !isplay;
+            setState(() {});
+          },
+          onSlideChange: (value) async {
+            await vpController.seekTo(
+              Duration(milliseconds: (value * videoDuration).toInt()),
+            );
+            this.setState(() {});
+          },
+        ),
+      ),
+    );
+
+    // 视频操作
+    Widget actions = Row(
+      children: <Widget>[
+        ActionButton(
+          color: Colors.blueAccent,
+          icon: Icons.pan_tool,
+          title: 'Stop P2P',
+          onTap: () async {
+            await Cdnbye.restartP2p();
+          },
+        ),
+        ActionButton(
+          color: Colors.blueAccent,
+          icon: Icons.cast_connected,
+          title: 'Restart P2P',
+          onTap: () async {
+            await Cdnbye.restartP2p();
+          },
+        ),
+        ActionButton(
+          color: Colors.orangeAccent,
+          icon: Icons.replay,
+          title: 'Replay',
+          onTap: () async {
+            position = 0;
+            await vpController.seekTo(Duration(seconds: 0));
+            vpController.play();
+          },
+        ),
+        ActionButton(
+          color: Colors.redAccent,
+          icon: Icons.settings_power,
+          title: 'Reload',
+          onTap: () async {
+            _loadVideo();
+          },
+        ),
+      ],
+    );
+
+    actions = Container(
+      color: Colors.white,
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 12),
+            child: Text(
+              'Actions:',
+              style: TextStyle(
+                color: Color(0xff9b9b9b),
+                fontSize: 12,
+              ),
+            ),
+          ),
+          Container(height: 4),
+          actions,
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('CDNBye Demo'),
       ),
       body: ListView(
         children: <Widget>[
-          Container(
-            color: Colors.black,
-            child: AspectRatio(
-              aspectRatio: 16 / 9.0,
-              child: VideoPlayerWidget(
-                video: video,
-                aspectRatio: aspectRatio,
-                videoDuration: videoDuration,
-                videoPositon: position,
-                isPlaying: isplay,
-                showToolLayer: showUi,
-                onTap: () async {
-                  if (!vpController.value.initialized) {
-                    return;
-                  }
-                  showUi = !showUi;
-                  setState(() {});
-                  if (showUi && isplay) {
-                    await Future.delayed(Duration(seconds: 6));
-                    showUi = false;
-                    setState(() {});
-                  }
-                },
-                onTapPlay: () async {
-                  if (!vpController.value.initialized) {
-                    return;
-                  }
-                  if (isplay) {
-                    vpController.pause();
-                  } else {
-                    vpController.play();
-                  }
-                  isplay = !isplay;
-                  setState(() {});
-                },
-                onSlideChange: (value) async {
-                  await vpController.seekTo(
-                    Duration(milliseconds: (value * videoDuration).toInt()),
-                  );
-                  this.setState(() {});
-                },
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(right: 12),
-                  child: Text(
-                    'Actions:',
-                    style: TextStyle(
-                      color: Color(0xff9b9b9b),
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-                Container(height: 4),
-                Row(
-                  children: <Widget>[
-                    ActionButton(
-                      color: Colors.blueAccent,
-                      icon: Icons.pan_tool,
-                      title: 'Stop P2P',
-                      onTap: () async {
-                        await Cdnbye.restartP2p();
-                      },
-                    ),
-                    ActionButton(
-                      color: Colors.blueAccent,
-                      icon: Icons.cast_connected,
-                      title: 'Restart P2P',
-                      onTap: () async {
-                        await Cdnbye.restartP2p();
-                      },
-                    ),
-                    ActionButton(
-                      color: Colors.orangeAccent,
-                      icon: Icons.replay,
-                      title: 'Replay',
-                      onTap: () async {
-                        position = 0;
-                        await vpController.seekTo(Duration(seconds: 0));
-                        vpController.play();
-                      },
-                    ),
-                    ActionButton(
-                      color: Colors.redAccent,
-                      icon: Icons.settings_power,
-                      title: 'Reload',
-                      onTap: () async {
-                        _loadVideo();
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          // Container(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text('info:\n$_info'),
-          // ),
+          topVideo,
+          actions,
           Container(height: 12),
           InfoRow(
             k1: 'Http Download',
@@ -249,7 +253,7 @@ class _CustomVideoPlayerPageState extends State<CustomVideoPlayerPage> {
             k2: 'P2P Connected',
             v2: _connected,
           ),
-          Text("Peer ID " + _peerId),
+          Text("Peer ID:" + _peerId),
         ],
       ),
     );
@@ -385,145 +389,6 @@ class OneInfo extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class VideoPlayerWidget extends StatelessWidget {
-  final Widget video;
-  final double aspectRatio;
-  final int videoDuration;
-  final int videoPositon;
-  final bool showToolLayer;
-  final bool isPlaying;
-  // 事件
-  final Function onTap;
-  final Function onTapPlay;
-  final Function onTapFullScreen;
-  final Function(double) onSlideChange;
-
-  String get positionStr {
-    var sec = (videoDuration ~/ 1000) % 60;
-    var min = (videoDuration ~/ 1000) ~/ 60;
-    var nowSec = (videoPositon ~/ 1000) % 60;
-    var nowMin = (videoPositon ~/ 1000) ~/ 60;
-    return '$nowMin:$nowSec/$min:$sec';
-  }
-
-  const VideoPlayerWidget({
-    Key key,
-    this.video,
-    this.aspectRatio,
-    this.videoDuration,
-    this.videoPositon,
-    this.showToolLayer,
-    this.isPlaying,
-    this.onTap,
-    this.onTapPlay,
-    this.onTapFullScreen,
-    this.onSlideChange,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    double sliderValue = 0;
-    if (videoDuration != 0) {
-      sliderValue = videoPositon / videoDuration;
-
-      if (sliderValue <= 0) {
-        sliderValue = 0;
-      }
-      if (sliderValue >= 1) {
-        sliderValue = 1;
-      }
-    }
-
-    // 底部播放条
-    Widget bottom = Container(
-      width: double.infinity,
-      height: double.infinity,
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        width: double.infinity,
-        height: 36,
-        color: Colors.black.withOpacity(0.5),
-        padding: EdgeInsets.symmetric(horizontal: 12),
-        child: Row(
-          children: <Widget>[
-            Tapped(
-              child: Icon(
-                isPlaying ? Icons.pause : Icons.play_arrow,
-                color: Colors.white,
-              ),
-              onTap: onTapPlay,
-            ),
-            Expanded(
-              child: Container(
-                child: Slider(
-                  inactiveColor: Colors.white.withOpacity(0.3),
-                  activeColor: Colors.white,
-                  value: sliderValue,
-                  onChanged: onSlideChange,
-                ),
-              ),
-            ),
-            Container(
-              child: Text(
-                positionStr,
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: Tapped(
-                child: Icon(
-                  Icons.fullscreen,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-    Widget eventGesture = Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: GestureDetector(
-        onTap: onTap,
-      ),
-    );
-
-    Widget toolLayer = Stack(
-      children: <Widget>[
-        bottom,
-      ],
-    );
-    toolLayer = AnimatedOpacity(
-      opacity: showToolLayer ? 1 : 0,
-      duration: Duration(milliseconds: 200),
-      curve: Curves.bounceInOut,
-      child: toolLayer,
-    );
-
-    return Stack(
-      children: <Widget>[
-        Container(
-          alignment: Alignment.center,
-          child: AspectRatio(
-            aspectRatio: aspectRatio,
-            child: Container(
-              child: video,
-            ),
-          ),
-        ),
-        eventGesture,
-        Container(
-          // color: Colors.yellow,
-          height: double.infinity,
-          width: double.infinity,
-          child: toolLayer,
-        ),
-      ],
     );
   }
 }
---
+++ example/lib/views/videoPlayer.dart
@@ -0,0 +1,141 @@
+import 'package:cdnbye_example/views/tapped.dart';
+import 'package:flutter/material.dart';
+
+class VideoPlayerWidget extends StatelessWidget {
+  final Widget video;
+  final double aspectRatio;
+  final int videoDuration;
+  final int videoPositon;
+  final bool showToolLayer;
+  final bool isPlaying;
+  // 事件
+  final Function onTap;
+  final Function onTapPlay;
+  final Function onTapFullScreen;
+  final Function(double) onSlideChange;
+
+  String get positionStr {
+    var sec = (videoDuration ~/ 1000) % 60;
+    var min = (videoDuration ~/ 1000) ~/ 60;
+    var nowSec = (videoPositon ~/ 1000) % 60;
+    var nowMin = (videoPositon ~/ 1000) ~/ 60;
+    return '$nowMin:$nowSec/$min:$sec';
+  }
+
+  const VideoPlayerWidget({
+    Key key,
+    this.video,
+    this.aspectRatio,
+    this.videoDuration,
+    this.videoPositon,
+    this.showToolLayer,
+    this.isPlaying,
+    this.onTap,
+    this.onTapPlay,
+    this.onTapFullScreen,
+    this.onSlideChange,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    double sliderValue = 0;
+    if (videoDuration != 0) {
+      sliderValue = videoPositon / videoDuration;
+
+      if (sliderValue <= 0) {
+        sliderValue = 0;
+      }
+      if (sliderValue >= 1) {
+        sliderValue = 1;
+      }
+    }
+
+    // 底部播放条
+    Widget bottom = Container(
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: double.infinity,
+        height: 36,
+        color: Colors.black.withOpacity(0.5),
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Row(
+          children: <Widget>[
+            Tapped(
+              child: Icon(
+                isPlaying ? Icons.pause : Icons.play_arrow,
+                color: Colors.white,
+              ),
+              onTap: onTapPlay,
+            ),
+            Expanded(
+              child: Container(
+                child: Slider(
+                  inactiveColor: Colors.white.withOpacity(0.3),
+                  activeColor: Colors.white,
+                  value: sliderValue,
+                  onChanged: onSlideChange,
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                positionStr,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Tapped(
+                child: Icon(
+                  Icons.fullscreen,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    Widget eventGesture = Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: GestureDetector(
+        onTap: onTap,
+      ),
+    );
+
+    Widget toolLayer = Stack(
+      children: <Widget>[
+        bottom,
+      ],
+    );
+    toolLayer = AnimatedOpacity(
+      opacity: showToolLayer ? 1 : 0,
+      duration: Duration(milliseconds: 200),
+      curve: Curves.bounceInOut,
+      child: toolLayer,
+    );
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          child: AspectRatio(
+            aspectRatio: aspectRatio,
+            child: Container(
+              child: video,
+            ),
+          ),
+        ),
+        eventGesture,
+        Container(
+          // color: Colors.yellow,
+          height: double.infinity,
+          width: double.infinity,
+          child: toolLayer,
+        ),
+      ],
+    );
+  }
+}

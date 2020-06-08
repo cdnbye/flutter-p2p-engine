86fb8c621130f82752f35b896ae1ea9e25900108
MJL <675531696@qq.com>
2020-06-08 18:19:36 +0800

    修复闪退问题

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/majialun/Documents/flutter/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/majialun/Desktop/code/P2P-Engine/flutter-p2p-engine/example"
-export "FLUTTER_TARGET=/Users/majialun/Desktop/code/P2P-Engine/flutter-p2p-engine/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/majialun/Documents/flutter/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.5.0"
+export "FLUTTER_BUILD_NAME=1.6.0"
 export "FLUTTER_BUILD_NUMBER=3"
-export "TRACK_WIDGET_CREATION=true"
